d7b346653b9f74a8bc2c854376ba2a14197a8916
Emmanuel Odeke <emm.odeke@gmail.com>
2017-04-24 18:09:42 -0600

    implemented a client with methods AddCard, Charge
    Implemented an API client that allows you to add a card
    as well as charge someone for payments.
    
    My usecase here is for lunch time services and might integrate
    with Apple Pay since my school's lunchtime queues are one
    hell of a mess. With the sample app, I'll be able to have
    students reimburse their friends at the front of the queue for
    any purchases.
    
    See the examples in example_test.go

---
+++ example_test.go
@@ -0,0 +1,93 @@
+package securionpay_test
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/odeke-em/securionpay"
+)
+
+func Example_client_AddCard() {
+	client, err := securionpay.NewClientFromEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	card, err := client.AddCard(&securionpay.AddCardRequest{
+		CustomerID: "johnDoeCustomerID",
+		Card: &securionpay.Card{
+			ID:    "card_8P7OWXA5xiTS1ISnyZcum1KV",
+			Brand: "Visa",
+
+			CreatedAt:   1415810511,
+			FingerPrint: "e3d8suyIDgFg3pE7",
+			ExpiryMonth: 11,
+			ExpiryYear:  2022,
+			CustomerID:  "cust_AoR0wvgntQWRUYMdZNLYMz5R",
+
+			First6Digits: "424242",
+			Last4Digits:  "4242",
+
+			CardHolderName: "John Doe",
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("newly registered card: %#v\n", card)
+}
+
+func Example_client_Charge() {
+	client, err := securionpay.NewClientFromEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	charge, err := client.Charge(&securionpay.Charge{
+		Description: "Lunch reimbursement",
+
+		AmountMinorCurrencyUnits: 1500, // Amount of $15 in cents
+
+		Billing: &securionpay.Billing{
+			Address: &securionpay.Address{
+				Country: "USA",
+				City:    "Washington",
+				State:   "Washington DC",
+				Line1:   "1600 Pennsylvania Ave NW",
+				Zip:     "20500",
+			},
+		},
+
+		Card: &securionpay.Card{
+			ID:    "card_8P7OWXA5xiTS1ISnyZcum1KV",
+			Brand: "Visa",
+
+			CreatedAt:   1415810511,
+			FingerPrint: "e3d8suyIDgFg3pE7",
+			ExpiryMonth: 11,
+			ExpiryYear:  2022,
+			CustomerID:  "cust_AoR0wvgntQWRUYMdZNLYMz5R",
+
+			First6Digits: "424242",
+			Last4Digits:  "4242",
+
+			CardHolderName: "John Doe",
+		},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("charge processed: %#v\n", charge)
+}
+
+func Example_client_fromEnv() {
+	client, err := securionpay.NewClientFromEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("client: %#v\n", client)
+}
